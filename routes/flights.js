3211cf333c93161618837937aa041c4d2c191af4
hoken92 <hkenneth92@gmail.com>
2024-11-20 14:53:23 -0800

    commented out pagination

--- routes/flights.js
+++ routes/flights.js
@@ -11,8 +11,8 @@ const router = express.Router();
 router.get("/", async (req, res, next) => {
   try {
     // If req.query is empty, return an error
-    if (!req.query) {
-      res.status(400).send("Query cannot be empty.");
+    if (!req.body) {
+      res.status(400).send("Request body cannot be empty.");
       return;
     }
 
@@ -22,9 +22,33 @@ router.get("/", async (req, res, next) => {
       clientSecret: process.env.APISECRET,
     });
 
-    const data = await amadeus.shopping.flightOffersSearch.get(req.query);
+    // Pagination from req.query
+    // const page = parseInt(req.query.page);
+    // const limit = parseInt(req.query.limit);
+    // const startIndex = (page - 1) * limit;
+    // const endIndex = page * limit;
+    // const results = {};
 
-    res.status(200).json(data);
+    // No pages display if no other data exists past limit
+    // if (endIndex < model.length) {
+    //   results.next = {
+    //     page: page + 1,
+    //     limit: limit,
+    //   };
+    // }
+
+    // if (startIndex > 0) {
+    //   results.previous = {
+    //     page: page - 1,
+    //     limit: limit,
+    //   };
+    // }
+
+    // Response from Amadeus API
+    const response = await amadeus.shopping.flightOffersSearch.get(req.body);
+    // results.results = response.data.slice(startIndex, endIndex);
+
+    res.status(200).json(response.data);
   } catch (err) {
     res.status(400).send(err);
   }
